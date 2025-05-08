646bab2014baabe1ea2215da440f012c66d9c96c
pps10711239 <adrlopola@alu.edu.gva.es>
2025-05-09 01:24:27 +0200

    Proyecto de calculadora con pruebas unitarias

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+class Calculadora:
+    def multiplicar(self, a, b):
+        return a * b
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 3:
+        print("Uso: python3 calculadora.py <num1> <num2>")
+        sys.exit(1)
+    num1 = float(sys.argv[1])
+    num2 = float(sys.argv[2])
+    calc = Calculadora()
+    resultado = calc.multiplicar(num1, num2)
+    print("Resultado:", resultado)
